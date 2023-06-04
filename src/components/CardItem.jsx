f6e046d5f10a33958092c1be6ecd252295aa7ce8
alekoyunja <alikoyuncu13@gmail.com>
2023-06-04 18:28:51 +0300

    Context API added

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,10 +1,14 @@
 import { FiShoppingCart } from "react-icons/fi";
+import { MyContext } from "../main";
+import { useContext } from "react";
 
 const CardItem = ({cart}) => {
+const {myState,setMyState}=useContext(MyContext)
   return (
     
 <div className="relative">
-        <FiShoppingCart className="text-2xl" />
+        <FiShoppingCart className="text-2xl" onClick={()=>setMyState("Alleko")}/>
+        {myState}
         {cart.length>0 && <span className="bg-red-500 text-white flex w-5 h-5 justify-center 
         items-center rounded-full absolute -top-2 -right-4 text-xs">{cart.length}</span>
       }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//context olustur ve export et kı ıstedın yerden ulasabıl
+export const MyContext=React.createContext()
+
+const MyProvider=({children})=>{
+
+ const [myState,setMyState]=useState("Ali") 
+//value altında ıstedın degıskenlerı tanımla
+  const value={
+    myState,
+    setMyState,
+  }
+
+  return <MyContext.Provider value={value}>{children}</MyContext.Provider>;
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <MyProvider>
     <App />
+    </MyProvider>
   </React.StrictMode>,
 )
